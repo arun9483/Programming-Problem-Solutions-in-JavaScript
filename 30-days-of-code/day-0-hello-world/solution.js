dbcd7b3cc13098f95c9d85351933de64b1c05b91
Arun Tripathi <arun9483@gmail.com>
2018-06-18 22:23:59 +0530

    solved 'Day 0: Hello, World problem.' of 30 Days of Code.

---
+++ 30-days-of-code/day-0-hello-world/solution.js
@@ -0,0 +1,22 @@
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+
+    // Write the second line of output that prints the contents of 'inputString' here.
+    console.log(inputString);
+}
+
+
+// Below commented code is read-only, displayed by default by editor
+/*
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
+*/
--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 1. Solve Me First
     - [Problem](https://www.hackerrank.com/challenges/solve-me-first/problem)
     - [Solution](./solve-me-first/solution.js)
+
+## Programming Problems: 30 Days of Code
+
+1. Day 0: Hello, World.
+    - [Problem](https://www.hackerrank.com/challenges/30-hello-world/problem)
+    - [Solution](./30-days-of-code/day-0-hello-world/solution.js)
